c1a013a92b97f0b4ef4fd2ff94613ed61db989f4
benhirsch24 <hirschb@lab126.com>
2016-04-14 17:41:00 -0700

    Parsing fleshed out a bit
    I think I have the parsing down a bit more:
        MParser has a stack
            Each stack entry is a list of the previous positions before each
    action was taken
            There's a macro to wrap parsing each atom; Each time you call it
    it pushes onto the stack, and if it fails it pops off and returns an Err
    
    It's definitely not done yet, but it's on its way. I probably need to
    just start writing with it and parsing a few atoms to see how it ends up
    feeling.

--- src/atoms.rs
+++ src/atoms.rs
@@ -1,17 +1,104 @@
-use parser::MParser;
+use parser::*;
 
 pub trait AtomParser<T> {
     fn parse(parser: &mut MParser) -> Result<T, String>;
 }
 
+pub struct MovieHeaderAtom {
+    pub size: u32,
+    pub version: u8,
+    pub flags: u32,
+    pub creation_time: u32,
+    pub modification_time: u32,
+    pub time_scale: u32,
+    pub duration: u32,
+    pub rate: f32,
+    pub volume: f32,
+    pub matrix: Vec<f32>,
+    pub preview_time: u32,
+    pub preview_duration: u32,
+    pub poster_time: u32,
+    pub selection_time: u32,
+    pub selection_duration: u32,
+    pub current_time: u32,
+    pub next_track_id: u32
+}
+
+impl MovieHeaderAtom {
+    fn default() -> MovieHeaderAtom {
+        MovieHeaderAtom {
+            size: 0,
+            version: 0,
+            flags: 0,
+            creation_time: 0,
+            modification_time: 0,
+            time_scale: 0,
+            duration: 0,
+            rate: 0.0f32,
+            volume: 0.0f32,
+            matrix: Vec::new(),
+            preview_time: 0,
+            preview_duration: 0,
+            poster_time: 0,
+            selection_time: 0,
+            selection_duration: 0,
+            current_time: 0,
+            next_track_id:0
+        }
+    }
+}
+
+macro_rules! atom_type {
+    ( $e:expr, $parser:ident ) => {
+        {
+            let typ = try!(TypeParserAction::try_parse($parser));
+            if typ != $e.to_string() {
+                return Err(format!("Type of atom didn't match; expected \"mvhd\" got {}", typ));
+            }
+            typ
+        }
+    }
+}
+
+impl AtomParser<MovieHeaderAtom> for MovieHeaderAtom {
+    fn parse(parser: &mut MParser) -> Result<MovieHeaderAtom, String> {
+        let mut movie_header = MovieHeaderAtom::default();
+
+        let size = try!(U32ParserAction::try_parse(parser));
+        let typ = atom_type!("mvhd", parser);
+        let version = try!(U32ParserAction::try_parse(parser));
+
+        unimplemented!();
+    }
+}
+
 pub struct MoovAtom {
-    pub size: u32
+    pub size: u32,
+    pub mvhd: Option<MovieHeaderAtom>
 }
 
 impl MoovAtom {
-    fn new(size: u32) -> MoovAtom {
+    fn new(size: u32, mvhd: Option<MovieHeaderAtom>) -> MoovAtom {
         MoovAtom {
-            size: size
+            size: size,
+            mvhd: mvhd
+        }
+    }
+}
+
+macro_rules! parse_atom {
+    ( $atom:ident, $parser:ident ) => {
+        {
+            $parser.push_stack();
+            try!(
+                match $atom::parse($parser) {
+                Ok(a)  => Ok(a),
+                Err(e) => {
+                    $parser.unwind();
+                    Err(e)
+                }
+                }
+            );
         }
     }
 }
@@ -23,6 +110,8 @@ impl AtomParser<MoovAtom> for MoovAtom {
             None    => return Err("Couldn't read moov size".to_string())
         };
 
-        return Ok(MoovAtom::new(size));
+        let mvhd_atom = parse_atom!(MovieHeaderAtom, parser);
+
+        return Ok(MoovAtom::new(size, None));
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -2,12 +2,88 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::str;
 
+macro_rules! check_length {
+    ( $x:expr, $p:ident, $t:expr ) => {
+        if $p.get_remaining_bytes() < $x {
+            return Err(format!("Not enough bytes for {}; need {} have {}", $t, $x, $p.get_remaining_bytes()));
+        }
+    };
+}
+
+
+pub trait ParserAction<T> {
+    fn try_parse(parser: &mut MParser) -> Result<T, String>;
+}
+
+pub struct TypeParserAction;
+impl ParserAction<String> for TypeParserAction {
+    fn try_parse(parser: &mut MParser) -> Result<String, String> {
+        check_length!(4, parser, "TypeParserAction");
+
+        let mut bytes = Vec::new();
+        bytes.push(parser.get_byte(0));
+        bytes.push(parser.get_byte(1));
+        bytes.push(parser.get_byte(2));
+        bytes.push(parser.get_byte(3));
+
+        match String::from_utf8(bytes) {
+            Ok(s)  => {
+                println!("String parsed: {}", s);
+                parser.move_cursor(4);
+
+                Ok(s)
+            },
+            Err(e) => Err(format!("Error parsing type string: {}", e))
+        }
+    }
+}
+
+pub struct U32ParserAction;
+impl ParserAction<u32> for U32ParserAction {
+    fn try_parse(parser: &mut MParser) -> Result<u32, String> {
+        check_length!(4, parser, "U32ParserAction");
+
+        let mut r : u32 = 0;
+
+        r = r | (parser.get_byte(3) as u32) << 0;
+        r = r | (parser.get_byte(2) as u32) << 8;
+        r = r | (parser.get_byte(1) as u32) << 16;
+        r = r | (parser.get_byte(0) as u32) << 24;
+
+        parser.move_cursor(4);
+
+        Ok(r)
+    }
+}
+
+pub struct U8ParserAction;
+impl ParserAction<u8> for U8ParserAction {
+    fn try_parse(parser: &mut MParser) -> Result<u8, String> {
+        check_length!(1, parser, "U8ParserAction");
+
+        let r = parser.get_byte(0);
+
+        parser.move_cursor(1);
+
+        Ok(r)
+    }
+}
+
 pub struct MParser {
     position: usize,
-    data: Vec<u8>
+    data: Vec<u8>,
+    history: Vec<Vec<usize>>
 }
 
 impl MParser {
+    pub fn get_position(&self) -> usize { self.position }
+
+    pub fn get_remaining_bytes(&self) -> usize { self.data.len() - self.position }
+
+    pub fn get_byte(&self, offset: usize) -> u8 {
+        self.data[self.position + offset]
+    }
+
     pub fn new(filename: &str) -> Result<MParser, String> {
         let mut file = match File::open(filename) {
             Ok(f)  => f,
@@ -34,11 +110,23 @@ impl MParser {
                  moov_pos,
                  str::from_utf8(chars).unwrap());
 
-        Ok(MParser{ position: moov_pos - 4, data: data})
+        Ok(MParser{ position: moov_pos - 4, data: data, history: Vec::new() })
     }
 
     pub fn move_cursor(&mut self, delta: isize) {
         self.position = ((self.position as isize) + delta) as usize;
+    }
+
+    pub fn push_stack(&mut self) {
+        self.history.push(Vec::new());
+    }
+
+    pub fn unwind(&mut self) {
+        let last_delta = self.history.last_mut().and_then(|v| v.pop());
+        match last_delta {
+            Some(delta) => self.position = self.position - delta,
+            None => {}
+        }
     }
 
     pub fn read_u32(&mut self) -> Option<u32> {
